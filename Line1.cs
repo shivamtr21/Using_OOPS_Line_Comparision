55fba13619e38b2ee3177a6cd962a670e18cc277
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-26 09:47:09 +0530

    [shivam tripathi] : Added Line Comparision Using Object uc2

--- Line1.cs
+++ Line1.cs
@@ -10,6 +10,7 @@ namespace UsingOOPS_lineComparision
     {
         public void Call()
         {
+            //Line1
             Console.WriteLine("Enter the value of x-cordinate of point1: x1");
             int x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the value of x-cordinate of point1: y1");
@@ -19,8 +20,31 @@ namespace UsingOOPS_lineComparision
             Console.WriteLine("Enter the value of x-cordinate of point1: y2");
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double length = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-            Console.WriteLine("Distance between two point: " + "(" + x1 + ", " + y1 + ")" + "(" + x2 + ", " + y2 + ") is :" + length);
+            double length1 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+            //Line2
+            Console.WriteLine("Enter the value of x-cordinate of point1: x3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x-cordinate of point1: y3");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x-cordinate of point1: x4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x-cordinate of point1: y4");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            double length2 = Math.Sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));
+            Console.WriteLine("Distance between two point: " + "(" + x1 + ", " + y1 + ")" + "(" + x2 + ", " + y2 + ") is :" + length1);
+            Console.WriteLine("Distance between two point: " + "(" + x3 + ", " + y3 + ")" + "(" + x4 + ", " + y4 + ") is :" + length2);
+
+            if (length1 == length2)
+            {
+                Console.WriteLine("Both The Line are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both The Line are not Equal");
+            }
+
         }
     }
 }
